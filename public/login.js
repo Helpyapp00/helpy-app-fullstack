b23211ee2b2b4c018d6a6c79bbf0041d727e7bcf
Fratides <helpyapp00@gmail.com>
2025-07-29 22:59:17 -0300

    atualizando backend

--- public/login.js
+++ public/login.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function() {
         showMessage('Verificando credenciais...', 'info');
 
         try {
-            const response = await fetch('https://www.helpyapp.net/api', {
+            const response = await fetch('https://www.helpyapp.net/api/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
